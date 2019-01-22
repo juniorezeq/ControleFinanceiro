e64efa3612b02bcd2df3796f021cb496da3cf333
Junior <juniorezeq@hotmail.com>
2019-01-22 10:35:54 -0200

    Inicial commit

---
+++ src/test/java/br/com/financeiro/modelo/negocio/ContaReceber.java
@@ -0,0 +1,71 @@
+package br.com.financeiro.modelo.negocio;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_contareceber")
+public class ContaReceber implements Serializable,Lancamento {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_contareceber")
+	private Long id;
+	@Column(name = "descricao", length = 80, nullable = false)
+	private String descricao;
+	@Column(name = "valor",  nullable = false)
+	private float valor;
+	@Column(name = "vencimento", insertable = true, updatable = true, nullable = false)
+	private LocalDate vencimento;
+	
+	
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+
+	public LocalDate getVencimento() {
+		return vencimento;
+	}
+
+	public void setVencimento(LocalDate vencimento) {
+		this.vencimento = vencimento;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return  descricao + " Valor: " + valor;
+	}
+	
+	
+
+}
---
+++ src/test/java/br/com/financeiro/modelo/negocio/Lancamento.java
@@ -0,0 +1,10 @@
+package br.com.financeiro.modelo.negocio;
+
+import java.time.LocalDate;
+
+public interface Lancamento {
+
+	public String getDescricao();
+	public LocalDate getVencimento();
+	public float getValor();
+}
